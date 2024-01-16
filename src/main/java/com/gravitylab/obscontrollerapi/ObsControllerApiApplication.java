47e8d275ab1c9beb163706e5b199e7874df17e44
Achraf Labidi <achraf.labidi@tum.de>
2024-01-16 10:59:36 +0100

    Init project

---
+++ src/main/java/com/gravitylab/obscontrollerapi/ObsControllerApiApplication.java
@@ -0,0 +1,13 @@
+package com.gravitylab.obscontrollerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObsControllerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObsControllerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gravitylab/obscontrollerapi/ObsControllerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gravitylab.obscontrollerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObsControllerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
