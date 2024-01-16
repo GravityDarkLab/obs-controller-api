0406f22dca2d926783988e9ce0ccf6ebc597b2ea
Achraf Labidi <achraf.labidi@tum.de>
2024-01-16 12:18:25 +0100

    fix maven

--- src/main/java/com/gravitylab/obscontrollerapi/ObsControllerApiApplication.java
+++ src/main/java/com/gravitylab/obscontrollerapi/ObsControllerApiApplication.java
@@ -3,26 +3,11 @@ package com.gravitylab.obscontrollerapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * The main class of the application.
- */
 @SpringBootApplication
 public class ObsControllerApiApplication {
 
-	/**
-	 * The main method of the application.
-	 *
-	 * @param args The command line arguments.
-	 */
-	public static void main(final String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(ObsControllerApiApplication.class, args);
-	}
-
-	/**
-	 * This is a utility class. No instances of this class should be created.
-	 */
-	private ObsControllerApiApplication() {
-		throw new IllegalStateException("Utility class");
 	}
 
 }
